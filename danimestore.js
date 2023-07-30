7f4716728df4e8aaaba05cfee4d6b31657a18583
atori <x90.empty@outlook.com>
2023-07-30 15:52:03 +0900

    Support d-Animestore

---
+++ danimestore.js
@@ -0,0 +1,80 @@
+var DAnimestoreSync = class {
+	constructor() {
+		this.video = document.querySelector('video');
+	}
+
+	getDuration() {
+		return this.video.duration;
+	}
+
+	play() {
+		this.video.play();
+	}
+
+	pause() {
+		this.video.pause();
+	}
+
+	seekTo(position) {
+		this.video.currentTime = position;
+	}
+
+	seekAfter(sec) {
+		this.video.currentTime += sec;
+	}
+
+	sync(position) {
+		if (position < 0 || position > this.getDuration()) {
+			return;
+		}
+		this.seekTo(position);
+	}
+
+	sendPlaybackPosition() {
+		this.sendMessage('playbackPosition');
+	}
+
+	responsePosition() {
+		this.sendMessage('responsePosition');
+	}
+
+	sendMessage(command) {
+		chrome.runtime.sendMessage({
+			type: 'FROM_PAGE',
+			command: command,
+			data: {
+				position: this.video.currentTime,
+				currentTime: (new Date()).toISOString(),
+				mediaURL: document.URL,
+			},
+		}, undefined);
+	}
+
+	async sleep(ms) {
+		return new Promise(resolve => {
+			setTimeout(resolve, ms);
+		})
+	}
+}
+
+var syncCtl;
+
+const initializeSyncCtl = _ => {
+	if(location.pathname.startsWith('/animestore/sc_d_pc')) {
+		console.log('syncCtl is initialized.')
+		syncCtl = new DAnimestoreSync();
+	}
+};
+
+if(location.pathname.startsWith('/animestore/sc_d_pc')) {
+	const wait = setInterval(_ => {
+		if(document.querySelector('video')) {
+			initializeSyncCtl();
+			clearInterval(wait);
+		}
+	}, 100);
+	setTimeout(_ => {
+		clearInterval(wait);
+	}, 60000);
+}
+
